af4d926762bbc390eb801089c82ce1658bdc6898
I435726 <i435726@fhict.nl>
2021-01-02 18:33:33 +0100

    added passing Account user unit tests and integration tests

---
+++ Backend/Mobeye_API/Mobeye_API_Tests/DtosTest/AccountUserCreateDtoTest.cs
@@ -0,0 +1,197 @@
+﻿using Mobeye_API.Dtos;
+using Mobeye_API.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Mobeye_API_Tests.DtosTest
+{
+    public class AccountUserCreateDtoTest
+    {
+        private AccountUserCreateDto _accountUser;
+
+        public AccountUserCreateDtoTest()
+        {
+            _accountUser = new AccountUserCreateDto()
+            {
+                PhoneIMEI = "123",
+                SMSCode = "12345",
+                RegistrationPrivateKey = "abcd",
+                AuthPrivateKey = "safe",
+                Role = "Account User",
+                Devices = null,
+                Alarms = null
+            };
+        }
+        [Fact]
+        public void GetPhoneIMEI()
+        {
+            Assert.Equal("123", _accountUser.PhoneIMEI);
+        }
+        [Fact]
+        public void SetPhoneIMEI()
+        {
+            _accountUser.PhoneIMEI = "987";
+            Assert.Equal("987", _accountUser.PhoneIMEI);
+        }
+        [Fact]
+        public void GetSMSCode()
+        {
+            Assert.Equal("12345", _accountUser.SMSCode);
+        }
+        [Fact]
+        public void SetSMSCode()
+        {
+            _accountUser.SMSCode = "0000";
+            Assert.Equal("0000", _accountUser.SMSCode);
+        }
+        [Fact]
+        public void GetRegistrationPrivateKey()
+        {
+            Assert.Equal("abcd", _accountUser.RegistrationPrivateKey);
+        }
+        [Fact]
+        public void SetRegistrationPrivateKey()
+        {
+            _accountUser.RegistrationPrivateKey = "qwert";
+            Assert.Equal("qwert", _accountUser.RegistrationPrivateKey);
+        }
+        [Fact]
+        public void GetAuthPrivateKey()
+        {
+            Assert.Equal("safe", _accountUser.AuthPrivateKey);
+        }
+        [Fact]
+        public void SetAuthPrivateKey()
+        {
+            _accountUser.AuthPrivateKey = "safe1";
+            Assert.Equal("safe1", _accountUser.AuthPrivateKey);
+        }
+        [Fact]
+        public void GetRole()
+        {
+            Assert.Equal("Account User", _accountUser.Role);
+        }
+        [Fact]
+        public void SetRole()
+        {
+            _accountUser.Role = "Contact User";
+            Assert.Equal("Contact User", _accountUser.Role);
+        }
+        [Fact]
+        public void GetDevices()
+        {
+            Assert.Null(_accountUser.Devices);
+        }
+        [Fact]
+        public void SetDevices()
+        {
+            List<Device> devices = new List<Device>();
+            Device newDevice = new Device();
+            newDevice.Id = "hello";
+            newDevice.Devicename = "alarmdetector";
+            newDevice.Devicename = "open";
+            devices.Add(newDevice);
+            ICollection<Device> allDevices = devices;
+            _accountUser.Devices = allDevices;
+            Assert.Equal(allDevices, _accountUser.Devices);
+
+        }
+        [Fact]
+        public void GetAlarms()
+        {
+            Assert.Null(_accountUser.Alarms);
+        }
+        [Fact]
+        public void SetAlarms()
+        {
+            List<Alarm> alarms = new List<Alarm>();
+            var _alarm = new Alarm()
+            {
+                Id = Guid.NewGuid(),
+                Devicename = "SmokeDetector",
+                Location = "Eindhoven",
+                Alarmtext = "Smoke has been detected",
+                Set_Reset = "Set",
+                Priority = 1,
+                TimeOfAlarm = DateTime.Today,
+                Value = "1.2",
+                MessageId = "1",
+                Recipients = null,
+                Escalation = false,
+                Confirmed_Denied = "Confirmed",
+                Confirmed_at = DateTime.Today
+            };
+            alarms.Add(_alarm);
+            ICollection<Alarm> allAlarms = alarms;
+            _accountUser.Alarms = allAlarms;
+            Assert.Equal(allAlarms, _accountUser.Alarms);
+        }
+        [Fact]
+        public void PhoneIMEITypeTest()
+        {
+            Assert.IsType<string>(_accountUser.PhoneIMEI);
+        }
+        [Fact]
+        public void SMSCodeTypeTest()
+        {
+            Assert.IsType<string>(_accountUser.SMSCode);
+        }
+        [Fact]
+        public void RegistrationPrivateKeyTypeTest()
+        {
+            Assert.IsType<string>(_accountUser.RegistrationPrivateKey);
+        }
+        [Fact]
+        public void AuthPrivateKeyTypeTest()
+        {
+            Assert.IsType<string>(_accountUser.AuthPrivateKey);
+        }
+        [Fact]
+        public void RoleTypeTest()
+        {
+            Assert.IsType<string>(_accountUser.Role);
+        }
+        [Fact]
+        public void DevicesTypeTest()
+        {
+            List<Device> devices = new List<Device>();
+            Device newDevice = new Device();
+            newDevice.Id = "hello";
+            newDevice.Devicename = "alarmdetector";
+            newDevice.Devicename = "open";
+            devices.Add(newDevice);
+            ICollection<Device> allDevices = devices;
+            _accountUser.Devices = allDevices;
+            Assert.IsAssignableFrom<ICollection<Device>>(_accountUser.Devices);
+        }
+        [Fact]
+        public void AlarmsTypeTest()
+        {
+
+            List<Alarm> alarms = new List<Alarm>();
+            var _alarm = new Alarm()
+            {
+                Id = Guid.NewGuid(),
+                Devicename = "SmokeDetector",
+                Location = "Eindhoven",
+                Alarmtext = "Smoke has been detected",
+                Set_Reset = "Set",
+                Priority = 1,
+                TimeOfAlarm = DateTime.Today,
+                Value = "1.2",
+                MessageId = "1",
+                Recipients = null,
+                Escalation = false,
+                Confirmed_Denied = "Confirmed",
+                Confirmed_at = DateTime.Today
+            };
+            alarms.Add(_alarm);
+            ICollection<Alarm> allAlarms = alarms;
+            _accountUser.Alarms = allAlarms;
+            Assert.IsAssignableFrom<ICollection<Alarm>>(_accountUser.Alarms);
+
+        }
+    }
+}
---
+++ Backend/Mobeye_API/Mobeye_API_Tests/RepositoryTest/AccountUserRepoTest.cs
@@ -0,0 +1,159 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Mobeye_API.Data;
+using Mobeye_API.Models;
+using Mobeye_API.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace Mobeye_API_Tests.RepositoryTest
+{
+    public class AccountUserRepoTest : IDisposable
+    {
+        //INTEGRATION TESTING
+        DbContextOptionsBuilder<ApplicationDBContext> optionsBuilder;
+        ApplicationDBContext dBContext;
+        AccountUserRepo _accountUserRepo;
+
+        public AccountUserRepoTest()
+        {
+            optionsBuilder = new DbContextOptionsBuilder<ApplicationDBContext>();
+            optionsBuilder.UseInMemoryDatabase("IntegrationTestInMemoDb");
+            dBContext = new ApplicationDBContext(optionsBuilder.Options);
+
+            _accountUserRepo = new AccountUserRepo(dBContext);
+        }
+        public void Dispose()
+        {
+            optionsBuilder = null;
+            foreach (var accountUser in dBContext.AccountUsers)
+            {
+                dBContext.AccountUsers.Remove(accountUser);
+            }
+            dBContext.SaveChanges();
+            dBContext.Dispose();
+            _accountUserRepo = null;
+        }
+        [Fact]
+        public void GetAccountUsersWithEmptyDB_ReturnsZeroItemsTest()
+        {
+            //Arrange
+            //Act
+            var result = _accountUserRepo.GetAllUsers("Account User");
+            //Assert
+            Assert.Empty(result);
+        }
+        [Fact]
+        public void GetAccountUsersWithOneUserInTheDBTest()
+        {
+            //Arrange
+            var accountUser = new AccountUser()
+            {
+                Id = Guid.NewGuid(),
+                PhoneIMEI = "123",
+                SMSCode = "12345",
+                RegistrationPrivateKey = "abcd",
+                AuthPrivateKey = "safe",
+                Role = "Account User",
+                Devices = null,
+                Alarms = null
+            };
+            dBContext.AccountUsers.Add(accountUser);
+            dBContext.SaveChanges();
+            //Act
+            var result = _accountUserRepo.GetAllUsers("Account User");
+            //Assert
+            Assert.Single(result);
+        }
+        [Fact]
+        public void GetAllAccountUsers_ReturnsCorrectTypeTest()
+        {
+            //Arrange
+            //Act
+            var result = _accountUserRepo.GetAllUsers("Account User");
+            //Assert
+            Assert.IsAssignableFrom<IEnumerable<User>>(result);
+        }
+        [Fact]
+        public void GetAccountUserWithInvalidIdTest_ShouldReturnNull()
+        {
+            //Arrange
+            //DB is empty, so any id would be invalid
+            //Act
+            var result = _accountUserRepo.GetUserById(Guid.NewGuid());
+            //Assert
+            Assert.Null(result);
+        }
+        [Fact]
+        public void GetAccountUserById_ReturnsTheCorrectTypeTest()
+        {
+            //Arrange
+            var accountUser = new AccountUser()
+            {
+                Id = Guid.NewGuid(),
+                PhoneIMEI = "123",
+                SMSCode = "12345",
+                RegistrationPrivateKey = "abcd",
+                AuthPrivateKey = "safe",
+                Role = "Account User",
+                Devices = null,
+                Alarms = null
+            };
+            dBContext.AccountUsers.Add(accountUser);
+            dBContext.SaveChanges();
+            var id = accountUser.Id;
+            //Act
+            var result = _accountUserRepo.GetUserById(id);
+            //Assert
+            Assert.IsType<AccountUser>(result);
+        }
+        [Fact]
+        public void GetAccountUserById_ReturnsCorrectResourceType()
+        {
+            //Arrange
+            var accountUser = new AccountUser()
+            {
+                Id = Guid.NewGuid(),
+                PhoneIMEI = "123",
+                SMSCode = "12345",
+                RegistrationPrivateKey = "abcd",
+                AuthPrivateKey = "safe",
+                Role = "Account User",
+                Devices = null,
+                Alarms = null
+            };
+            dBContext.AccountUsers.Add(accountUser);
+            dBContext.SaveChanges();
+            var id = accountUser.Id;
+            //Act
+            var result = _accountUserRepo.GetUserById(id);
+            //Assert
+            Assert.Equal(id, accountUser.Id);
+        }
+        [Fact]
+        public void AddAccountUserTest()
+        {
+            //Arrange
+            var accountUser = new AccountUser()
+            {
+                Id = Guid.NewGuid(),
+                PhoneIMEI = "123",
+                SMSCode = "12345",
+                RegistrationPrivateKey = "abcd",
+                AuthPrivateKey = "safe",
+                Role = "Account User",
+                Devices = null,
+                Alarms = null
+            };
+            var objCount = dBContext.AccountUsers.Count();
+            //Act
+            _accountUserRepo.CreateUser(accountUser);
+            _accountUserRepo.SaveChanges();
+            //Assert
+            Assert.Equal(objCount + 1, dBContext.AccountUsers.Count());
+        }
+
+    }
+}
