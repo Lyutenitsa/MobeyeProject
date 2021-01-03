f6352ea85747c5fb4ca3bc1b16e0a2dcf093ac27
I435726 <i435726@fhict.nl>
2021-01-03 16:56:36 +0100

    added requests classes

---
+++ Frontend/MobeyeApplication/MobeyeApplication/MobeyeApplication/MobeyeRESTClient/Requests/CheckAuthorizationRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MobeyeApplication.MobeyeRESTClient.Requests
+{
+    public class CheckAuthorizationRequest
+    {
+        //unique id of the phone -> IMEI demek
+        public string PhoneId { get; set; }
+        // first private key that was send with the registration request
+        public string PrivateKey { get; set; }
+    }
+}
