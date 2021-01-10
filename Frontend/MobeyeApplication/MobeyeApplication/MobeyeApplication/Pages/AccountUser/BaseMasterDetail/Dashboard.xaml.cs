040b63cb073bfe5f3cb525b3a27e65b958893fcd
I435726 <i435726@fhict.nl>
2021-01-10 18:41:18 +0100

    added Message Status for Account User - Confirm/Deny alarm

--- Frontend/MobeyeApplication/MobeyeApplication/MobeyeApplication/Pages/AccountUser/BaseMasterDetail/Dashboard.xaml.cs
+++ Frontend/MobeyeApplication/MobeyeApplication/MobeyeApplication/Pages/AccountUser/BaseMasterDetail/Dashboard.xaml.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -34,6 +34,35 @@ namespace MobeyeApplication.Pages.AccountUser.BaseMasterDetail
             else
             {
                 AlarmView.ItemsSource = content;
+            }
+        }
+        async void OnConfirmAlarmButtonClickedAsync(object sender, EventArgs args)
+        {
+            await RespondToAlarm("Confirmed");
+        }
+        async void OnDenyAlarmButtonClicked(object sender, EventArgs args)
+        {
+            await RespondToAlarm("Denied");
+        }
+        public async Task RespondToAlarm(string response)
+        {
+
+            var content = await API_Services.serviceClient.MessageStatus("aaaa1111", 1, response, "pkaaaa1111");
+            if (content == null)
+            {
+                await DisplayAlert("Alert", "Something went wrong! Please try again.", "Ok");
+            }
+            else if (response == "Confirmed")
+            {
+                await DisplayAlert("", "The Alarm was Confirmed", "OK");
+            }
+            else if (response == "Denied")
+            {
+                await DisplayAlert("", "The Alarm was Denied", "OK");
+            }
+            else
+            {
+                await DisplayAlert("", "Please try again", "OK");
             }
         }
     }
